23b7807f8678397b5075820115c7f6ebb09297b1
CStank89 <C.Stank89@gmail.com>
2020-09-01 15:32:40 -0400

    i have five down five to go

--- js/index.js
+++ js/index.js
@@ -1,1 +1,45 @@
-// Your code goes here
+// MOUSEOVER
+const test = document.querySelector("nav");
+  // This handler will be executed every time the cursor
+// is moved over a different list item
+test.addEventListener("mouseover", function( event ) {   
+    // highlight the mouseover target
+    event.target.style.color = "orange";
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+  }, false);
+
+
+
+//   KEYDOWN
+//   eventTarget.addEventListener("keydown", event => {
+//     if (event.isComposing || event.keyCode === 229) {
+//       return;
+//     }
+//     // do something
+//   });
+
+// WHEEL
+function zoom(event) {
+    event.preventDefault();
+  
+    scale += event.deltaY * -0.01;
+  
+    // Restrict scale
+    scale = Math.min(Math.max(.125, scale), 4);
+  
+    // Apply scale transform
+    el.style.transform = `scale(${scale})`;
+  }
+  
+  let scale = 1;
+  const el = document.querySelector('img');
+  el.onwheel = zoom;
+  
+//   LOAD
+window.addEventListener('load', (event) => {
+    alert('This is an alter from the window');
+  });
+
+// FOCUS
